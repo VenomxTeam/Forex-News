4a0fb0fd15963a977d6ec36539efcaa7c547ca12
darkdevil-dev <shortbreak233@gmail.com>
2026-07-28 23:24:05 +0530

    cleanup

--- check_cdn.py
+++
@@ -1,7 +0,0 @@
-import requests
-res = requests.get('https://cdn.jsdelivr.net/gh/VenomxTeam/Forex-News@main/scraper/news.json').json()
-if 'events' in res:
-    data = res['events']
-else:
-    data = res
-print([x['time'] for x in data if x['id'] == 'cad_cpi_mm_20260720'])
